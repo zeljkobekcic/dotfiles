6aa955bf232dd24d11cd3f5548622a0882185fcd
zeljkobekcic <bekciczeljko@gmail.com>
2017-11-10 23:39:12 +0100

    Removed unnecessary line in touchpad script

--- scripts/.scripts/toggle_touchpad.sh
+++ scripts/.scripts/toggle_touchpad.sh
@@ -4,8 +4,6 @@ dev_id=`xinput list | grep -i 'TouchPad' | awk '{ print $6 }' | sed 's/id=//'`
 
 cur_mode=`xinput list-props $dev_id | grep "Device Enabled" | tr ": " "\n" | tail -1`
 
-echo $cur_mode
-
 if [ $cur_mode -eq 1 ] ; then
     xinput --disable "$dev_id"
 else
