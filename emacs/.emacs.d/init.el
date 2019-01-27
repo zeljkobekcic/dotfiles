e3499b02b1f725d96b520b378c5c64c17508218f
zeljkobekcic <bekciczeljko@gmail.com>
2019-01-27 13:46:35 +0100

    Configuring emacs is fun

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,4 +1,4 @@
-;; Minimal UI
+; Minimal UI
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (tooltip-mode -1)
@@ -22,7 +22,39 @@
 
 (use-package evil
   :ensure t
-  :config (evil-mode 1))
+  :init
+  (setq evil-want-keybinding nil)
+  :config
+  (evil-mode 1))
+
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :config
+  (evil-collection-init))
+
+(use-package evil-numbers
+  :after evil
+  :ensure t
+  :config
+  (define-key evil-normal-state-map (kbd "C-c +") 'evil-numbers/inc-at-pt)
+  (define-key evil-visual-state-map (kbd "C-c +") 'evil-numbers/inc-at-pt)
+  (define-key evil-normal-state-map (kbd "C-c -") 'evil-numbers/dec-at-pt)
+  (define-key evil-visual-state-map (kbd "C-c -") 'evil-numbers/dec-at-pt))
+
+(use-package neotree
+  :ensure t
+  :config
+  (global-set-key [f8] 'neotree-toggle))
+
+(use-package volatile-highlights
+  :ensure t
+  :config (volatile-highlights-mode t))
+
+(use-package rainbow-delimiters
+  :ensure t
+  :config
+  (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
 
 (use-package helm
   :ensure t
@@ -31,16 +63,15 @@
   (setq helm-completion-in-region-fuzzy-match t)
   (setq helm-candidate-number-list 50))
 
-(use-package linum-relative
+(use-package nlinum-relative
+  :after evil
   :ensure t
   :config
-  (linum-relative-global-mode))
-
-(use-package flycheck
-  :ensure t
-  :config
-  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-  (add-hook 'elpy-mode-hook 'flycheck-mode))
+  (nlinum-relative-setup-evil)
+  (setq nlinum-relative-redisplay-delay 0)
+  (setq nlinum-relative-current-symbol "")
+  (setq nlinum-relative-offset 0)
+  (nlinum-relative-mode))
 
 (use-package gruvbox-theme 
   :ensure t
@@ -50,15 +81,19 @@
 (use-package elpy
   :ensure t
   :config
-  (elpy-enable)
   (setq python-shell-interpreter "ipython"
-      python-shell-interpreter-args "-i --simple-prompt"))
+        python-shell-interpreter-args "-i --simple-prompt"))
+
+(use-package flycheck
+  :ensure t
+  :config
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  :hook (elpy-mode . flycheck-mode))
 
 (use-package py-autopep8
   :ensure t
-  :config
-  (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save))
-
+  :hook
+  (elpy-mode py-autopep8-enable-on-save))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -70,7 +105,7 @@
     ("e2fd81495089dc09d14a88f29dfdff7645f213e2c03650ac2dd275de52a513de" "2a9039b093df61e4517302f40ebaf2d3e95215cb2f9684c8c1a446659ee226b9" "a622aaf6377fe1cd14e4298497b7b2cae2efc9e0ce362dade3a58c16c89e089c" default)))
  '(package-selected-packages
    (quote
-    (gruvbox-theme flycheck py-autopep8 elpy better-defaults solarized-theme poet-theme linum-relative helm use-package evil))))
+    (nlinum-relative volatile-highlights neotree evil-paredit evil-collection gruvbox-theme flycheck py-autopep8 elpy better-defaults solarized-theme poet-theme linum-relative helm use-package evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
