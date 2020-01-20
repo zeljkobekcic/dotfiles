0f6bfc4e03f482ad72b9e320961f1e2ab6820ac0
Zeljko Bekcic <bekciczeljko@gmail.com>
2020-01-20 18:24:12 +0100

    Felt like I should enforce the usage of aliases

--- zsh/.zsh/plugins.zsh
+++ zsh/.zsh/plugins.zsh
@@ -4,11 +4,19 @@ zplug "zdharma/fast-syntax-highlighting"
 zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-history-substring-search"
+# does not work so well with the completions or he history substring search
+# zplug "softmoth/zsh-vim-mode"
 zplug "mafredri/zsh-async", from:github
 zplug "sindresorhus/pure", use:pure.zsh, from:github, as:theme
+zplug "MichaelAquilina/zsh-you-should-use"
 
+zplug load
 
-zplug load --verbose
+#==============================================================================
+# ZSH ALIAS HARDCORE 
+#==============================================================================
+
+export YSU_HARDCORE=1
 
 #==============================================================================
 # ZSH SYNTAX HIGHLIGHTING
@@ -30,3 +38,12 @@ bindkey -M vicmd 'j' history-substring-search-down
 
 PURE_PROMPT_SYMBOL="[I]"
 PURE_PROMPT_VICMD_SYMBOL="[N]"
+
+#==============================================================================
+# ZSH HISTORY SUBSTRING SEARCH STUFF
+#==============================================================================
+
+MODE_CURSOR_VICMD="green block"
+MODE_CURSOR_VIINS="#20d08a blinking bar"
+MODE_CURSOR_SEARCH="#ff00ff blinking underline"
+
