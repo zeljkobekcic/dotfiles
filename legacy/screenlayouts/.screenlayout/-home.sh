f29e4dca09c069a6d49603962926321a4f1bf0a8
Zeljko Bekcic <bekciczeljko@gmail.com>
2020-04-30 16:23:33 +0200

    Remove old stuff

--- legacy/screenlayouts/.screenlayout/-home.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output eDP1 --mode 1920x1080 --pos 0x360 --rotate normal --output DP1 --off --output DP2 --off --output DP2-1 --primary --mode 2560x1440 --pos 1920x0 --rotate normal --output DP2-2 --off --output DP2-3 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
